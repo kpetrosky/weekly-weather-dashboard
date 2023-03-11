c63f952d3490652cd86a3f5a80c58043184fc8d2
kpetrosky <kpetrosky36@gmail.com>
2023-03-11 11:17:41 -0700

    trying to get the api to read correctly

--- scripts.js
+++ scripts.js
@@ -1,19 +1,34 @@
 var submit = document.getElementById("submit");
-var apiKey = '6c954c4894d6ffe51fdd55233b4b58b5'
+var apiKey = '6c954c4894d6ffe51fdd55233b4b58b5';
+var displayText = document.getElementById('display')
 submit.addEventListener('click', function() {
-    var url = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
-    console.log ("i made this")
+    var url = `https://api.openweathermap.org/data/2.5/forecast?lat={32.2540° N}&lon={110.9742° W}&appid=${apiKey}`;
+    console.log ("i made this");
     fetch(url)
     .then((response) => {
         if (response.ok) {
-            return response.json();
+            return response.JSON();
         } else {
             throw new Error("Something isn't working!");
         }
     })
     .then((data) => {
-        // handle successful response
-    })
+        render(data);
+    });
 });
 
-render ()
+function render(data) {
+    var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=Tucson&limit=5&appid={API key}';
+    fetch(geoUrl)
+    .then((response) => {
+        if (response.ok) {
+            return response.JSON();
+        } else {
+            throw new Error("Something isn't working!");
+        }
+    })
+    .then((data) => {
+        displayText.textContent = JSON.stringify(data);
+    });
+}
+
