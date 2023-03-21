5c8ca78995b53dd3ff51a025a35ed4a9ffb1372d
kpetrosky <kpetrosky36@gmail.com>
2023-03-20 18:32:30 -0700

    continue to try to make city search work within javascript

--- scripts.js
+++ scripts.js
@@ -11,26 +11,26 @@ fetchWeather.addEventListener("click", function () {
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      weekForcast(data.coord.lat,data.coord.lon)
+      weekForcast(data.coord.lat, data.coord.lon)
       var currentWeather = document.getElementById('current-weather');
-      var location =document.createElement('h2');
+      var location = document.createElement('h2');
       location.textContent = data.name;
       var temp = document.createElement('p');
       temp.textContent = `Temperature: ${data.main.temp} °F`;
-// same code for humidity, wind speed and anything else needed
-// temperature feels like
+      // same code for humidity, wind speed and anything else needed
+      // temperature feels like
       var feelsLike = document.createElement('p');
       feelsLike.textContent = `Feels like: ${data.main.feels_like} °F`
-   //humidity
-      var humidity =document.createElement ('p');
+      //humidity
+      var humidity = document.createElement('p');
       humidity.textContent = `Humidity: ${data.main.humidity} %`
       //wind 
       var windSpeed = document.createElement('p');
       windSpeed.textContent = `Wind:${data.wind.speed} MPH`
-      currentWeather.innerHTML ="";
-      currentWeather.append(location,temp,feelsLike,humidity,windSpeed);
+      currentWeather.innerHTML = "";
+      currentWeather.append(location, temp, feelsLike, humidity, windSpeed);
       //after creating new p tags dont forget to add in ln 22
-          })
+    })
     .catch(error => console.log(error));
 });
 
@@ -39,112 +39,86 @@ function weekForcast(lat, lon) {
   var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
 
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    var forecast = document.querySelector ('.forecast');
-    forecast.innerHTML = "";
-    for (let i = 0; i < data.list.length; i++) {
-      const element = data.list[i];
-      if (element.dt_txt.includes('15:00:00')) {
-        let article = document.createElement ('article');
-        article.setAttribute('class', 'day-forecast');
-        var date = document.createElement('h3')
-        date.textContent = element.dt_txt.split(' ')[0];
-        var temp =document.createElement('p')
-        temp.textContent = `Temperature: ${element.main.temp} °F`
-        // same code for humidity, wind speed and anything else needed
-        var feelsLike = document.createElement('p');
-      feelsLike.textContent = `Feels like: ${element.main.feels_like} °F`
-      //humidity
-      var humidity =document.createElement ('p');
-      humidity.textContent = `Humidity: ${element.main.humidity} %`
-      //wind 
-      var windSpeed = document.createElement('p');
-      windSpeed.textContent = `Wind:${element.wind.speed} MPH`
-        article.append (date,temp,feelsLike,humidity,windSpeed);
-        forecast.append (article);
-          //after creating new p tags dont forget to add in ln 57
-      }
-    }
-
-
-  })
-}
-
-
-
-var fetchCities = document.getElementsByClassName('fetchCities');
-
-    console.log(fetchCities);
-    var apiKey = "30286bdb0d1bd12bbf351dc0bda01e86"; // replace with your actual API key
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${fetchCities}&appid=${apiKey}&units=imperial`;
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        for (var i = 0; i < fetchCities.length; i++) {
-            fetchCities[i].addEventListener('click', function() {
-                // code for fetching weather data
-            });
-        }
-        weekForcast(data.coord.lat, data.coord.lon);
-        var currentWeather = document.getElementById('current-weather');
-        var location = document.createElement('h2');
-        location.textContent = data.name;
-        var temp = document.createElement('p');
-        temp.textContent = `Temperature: ${data.main.temp} °F`;
-        // same code for humidity, wind speed and anything else needed
-        // temperature feels like
-        var feelsLike = document.createElement('p');
-        feelsLike.textContent = `Feels like: ${data.main.feels_like} °F`;
-        // humidity
-        var humidity = document.createElement('p');
-        humidity.textContent = `Humidity: ${data.main.humidity} %`;
-        // wind 
-        var windSpeed = document.createElement('p');
-        windSpeed.textContent = `Wind: ${data.wind.speed} MPH`;
-        currentWeather.innerHTML = "";
-        currentWeather.append(location, temp, feelsLike, humidity, windSpeed);
-    })
-    .catch(error => console.log(error));
-
-      
-
-  function weekForcast(lat, lon) {
-    var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-  
-    fetch(url)
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      var forecast = document.querySelector ('.forecast');
+      var forecast = document.querySelector('.forecast');
       forecast.innerHTML = "";
       for (let i = 0; i < data.list.length; i++) {
         const element = data.list[i];
         if (element.dt_txt.includes('15:00:00')) {
-          let article = document.createElement ('article');
+          let article = document.createElement('article');
           article.setAttribute('class', 'day-forecast');
           var date = document.createElement('h3')
           date.textContent = element.dt_txt.split(' ')[0];
-          var temp =document.createElement('p')
+          var temp = document.createElement('p')
           temp.textContent = `Temperature: ${element.main.temp} °F`
           // same code for humidity, wind speed and anything else needed
           var feelsLike = document.createElement('p');
-        feelsLike.textContent = `Feels like: ${element.main.feels_like} °F`
-        //humidity
-        var humidity =document.createElement ('p');
-        humidity.textContent = `Humidity: ${element.main.humidity} %`
-        //wind 
-        var windSpeed = document.createElement('p');
-        windSpeed.textContent = `Wind:${element.wind.speed} MPH`
-          article.append (date,temp,feelsLike,humidity,windSpeed);
-          forecast.append (article);
-            //after creating new p tags dont forget to add in ln 57
+          feelsLike.textContent = `Feels like: ${element.main.feels_like} °F`
+          //humidity
+          var humidity = document.createElement('p');
+          humidity.textContent = `Humidity: ${element.main.humidity} %`
+          //wind 
+          var windSpeed = document.createElement('p');
+          windSpeed.textContent = `Wind:${element.wind.speed} MPH`
+          article.append(date, temp, feelsLike, humidity, windSpeed);
+          forecast.append(article);
+          //after creating new p tags dont forget to add in ln 57
         }
       }
-  
-  
+
+
     })
+}
+
+//making the other cities work
+
+var fetchCities = document.getElementsByClassName('fetchCities');
+
+// console.log(data);
+var apiKey = "30286bdb0d1bd12bbf351dc0bda01e86"; 
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${fetchCities}&appid=${apiKey}&units=imperial`;
+
+function weekForcast(lat, lon) {
+  var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+
+   
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        var forecast = document.querySelector('.forecast');
+        forecast.innerHTML = "";
+        console.log(data);
+        for (var i = 0; i < fetchCities.length; i++) {
+          fetchCities[i].addEventListener('click', function () {
+            // code for fetching weather data
+          });
+        }
+        data.list.forEach(element => {
+          if (element.dt_txt.includes('15:00:00')) {
+            let article = document.createElement('article');
+            article.setAttribute('class', 'day-forecast');
+            var date = document.createElement('h3')
+            date.textContent = element.dt_txt.split(' ')[0];
+            var temp = document.createElement('p')
+            temp.textContent = `Temperature: ${element.main.temp} °F`
+            // same code for humidity, wind speed and anything else needed
+            var feelsLike = document.createElement('p');
+            feelsLike.textContent = `Feels like: ${element.main.feels_like} °F`
+            //humidity
+            var humidity = document.createElement('p');
+            humidity.textContent = `Humidity: ${element.main.humidity} %`
+            //wind 
+            var windSpeed = document.createElement('p');
+            windSpeed.textContent = `Wind:${element.wind.speed} MPH`
+            article.append(date, temp, feelsLike, humidity, windSpeed);
+            forecast.append(article);
+            //after creating new p tags dont forget to add 
+          }
+        });
+      })
+      .catch(error => console.log(error));
   }
+  
