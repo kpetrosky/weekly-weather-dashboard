91fce1dee101c7111de423b49c726504fac36701
kpetrosky <kpetrosky36@gmail.com>
2023-03-22 17:38:55 -0700

    trying to get other cities to work, and started working on local storage. neither are working at this time.

--- scripts.js
+++ scripts.js
@@ -73,26 +73,45 @@ function weekForcast(lat, lon) {
 }
 
 //making the other cities work
-var fetchCities = document.getElementsByClassName('fetchCities');
-var cities = [
-  { name: "Atlanta", lat: "33.7490", lon: "-84.3880" },
-  { name: "Denver", lat: "39.7392", lon: "-104.9903" },
-  { name: "Seattle", lat: "47.6062", lon: "-122.3321" },
-  { name: "Orlando", lat: "28.5383", lon: "-81.3792" },
-  { name: "New York", lat: "40.7128", lon: "-74.0060" },
-  { name: "Chicago", lat: "41.8781", lon: "-87.6298" },
-  { name: "Austin", lat: "30.2672", lon: "-97.7431" }
-];
+var fetchCities = document.getElementById('fetchCities');
+var cityName = document.querySelector('city name');
+fetchCities.addEventListener("click", function (lat, lon) {
+  var cityName = [
+    { name: "Atlanta", lat: "33.7490", lon: "-84.3880" },
+    { name: "Denver", lat: "39.7392", lon: "-104.9903" },
+    { name: "Seattle", lat: "47.6062", lon: "-122.3321" },
+    { name: "Orlando", lat: "28.5383", lon: "-81.3792" },
+    { name: "New York", lat: "40.7128", lon: "-74.0060" },
+    { name: "Chicago", lat: "41.8781", lon: "-87.6298" },
+    { name: "Austin", lat: "30.2672", lon: "-97.7431" }
+  ];
 
 var apiKey = "30286bdb0d1bd12bbf351dc0bda01e86";
+// fetchCities.addEventListener("click", function (lat, lon) {
 
-function weekForcast(lat, lon) {
-  var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
 
-  fetch(url)
-  console.log('can you see me?')
-    .then(response => response.json())
+
+  for (var i = 0; i < fetchCities.length; i++) {
+    var city = cities.find(c => c.name === fetchCities[i].classList[0]);
+    fetchCities[i].setAttribute('data-lat', city.lat);
+    fetchCities[i].setAttribute('data-lon', city.lon);
     
+    fetchCities[i].addEventListener('click', function () {
+      // Get lat and lon for the clicked city
+      var lat = this.getAttribute('data-lat');
+      var lon = this.getAttribute('data-lon');
+      // Call the weekForcast function
+      weekForcast(lat, lon);
+    });
+  }
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
     .then(data => {
       console.log(data);
       var forecast = document.querySelector('.forecast');
@@ -104,7 +123,7 @@ function weekForcast(lat, lon) {
           article.setAttribute('class', 'day-forecast');
           
           var date = document.createElement('h3')
-          date.textContent = element.dt_txt.split(' ')[0];
+          date.textContent = element.dt_txt.split(' ')[i];
           var temp = document.createElement('p')
           temp.textContent = `Temperature: ${element.main.temp} °F`
           
@@ -122,27 +141,35 @@ function weekForcast(lat, lon) {
         }
       });
     })
-    .catch(error => console.log(error));
-}
+    .catch(error => console.log('There was a problem with the fetch operation:', error));
+})
 
-for (var i = 0; i < fetchCities.length; i++) {
-  var city = cities.find(c => c.name === fetchCities[i].classList[0]);
-  fetchCities[i].setAttribute('data-lat', city.lat);
-  fetchCities[i].setAttribute('data-lon', city.lon);
+
+// for (var i = 0; i < fetchCities.length; i++) {
+//   var city = cities.find(c => c.name === fetchCities[i].classList[0]);
+//   fetchCities[i].setAttribute('data-lat', city.lat);
+//   fetchCities[i].setAttribute('data-lon', city.lon);
   
-  fetchCities[i].addEventListener('click', function () {
-    // Get lat and lon for the clicked city
-    var lat = this.getAttribute('data-lat');
-    var lon = this.getAttribute('data-lon');
-    // Call the weekForcast function
-    weekForcast(lat, lon);
-  });
-}
+//   fetchCities[i].addEventListener('click', function () {
+//     // Get lat and lon for the clicked city
+//     var lat = this.getAttribute('data-lat');
+//     var lon = this.getAttribute('data-lon');
+//     // Call the weekForcast function
+//     weekForcast(lat, lon);
+//   });
+// }
 
 
-// Store a value in localStorage
-// define a function that processes some data
-// function processForecast(data) {
+// //Store a value in localStorage
+// //define a function that processes some data
+// function forecast(weeklyWeather) {
+//   const weeklyWeather = [i];
+//   for (let i = 0; i < data.list.length; i++) {
+//     const weeklyWeather = data.list[i];
+//   }
+// }
+// return data
+// // function processForecast(data) {
 //   var processedData = [];
 //   for (let i = 0; i < data.list.length; i++) {
 //     const element = data.list[i];
@@ -152,7 +179,7 @@ for (var i = 0; i < fetchCities.length; i++) {
 //   return processedData;
 // }
 
-// call the function and store the result in localStorage
+// //call the function and store the result in localStorage
 // var processedData = document.getElementById('day-forecast')
 // localStorage.setItem('forecast', JSON.stringify(processed));
 
